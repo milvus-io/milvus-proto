0e58cc7f5d3f7066e81f80f392abc93f67bca58a
SimFG <bang.fu@zilliz.com>
2022-10-14 15:53:14 +0800

    Add the `GetVersion` rpc
    Signed-off-by: SimFG <bang.fu@zilliz.com>

--- scripts/core_build.sh
+++ scripts/core_build.sh
@@ -4,6 +4,7 @@ SCRIPTS_DIR=$(dirname "$0")
 CMAKE_DIR="${SCRIPTS_DIR}/../cmake"
 BUILD_OUTPUT_DIR="${SCRIPTS_DIR}/../cmake-build"
 
+mkdir -p ${BUILD_OUTPUT_DIR}
 pushd ${BUILD_OUTPUT_DIR}
 cmake ${CMAKE_DIR}
 popd
