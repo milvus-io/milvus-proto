68612178cb2a572da1268fc8c8ae5f13839a0055
SimFG <bang.fu@zilliz.com>
2022-10-19 21:24:03 +0800

    Use built-in protoc to generate the pb file
    Signed-off-by: SimFG <bang.fu@zilliz.com>

--- scripts/core_build.sh
+++ scripts/core_build.sh
@@ -7,4 +7,21 @@ BUILD_OUTPUT_DIR="${SCRIPTS_DIR}/../cmake-build"
 mkdir -p ${BUILD_OUTPUT_DIR}
 pushd ${BUILD_OUTPUT_DIR}
 cmake ${CMAKE_DIR}
+
+if [[ ! ${jobs+1} ]]; then
+    if command -v nproc &> /dev/null
+    # For linux
+    then
+        jobs=$(nproc)
+    elif command -v sysctl &> /dev/null
+    # For macOS
+    then
+        jobs=$(sysctl -n hw.logicalcpu)
+    else
+        jobs=4
+    fi
+fi
+
+make -j ${jobs} protoc
+make -j ${jobs} install/local
 popd
